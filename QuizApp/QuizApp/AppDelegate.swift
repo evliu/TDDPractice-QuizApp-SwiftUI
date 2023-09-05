09d5340a1bb1a6a46c01b03fa7a935042fb3d82a
Everest Liu <ev@logik-m2-mini.local>
2023-09-05 16:52:59 -0700

    QuizApp: Testing ViewController in Swift

---
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  QuizApp
+//
+//  Created by Everest Liu on 9/5/23.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		return true
+	}
+}
