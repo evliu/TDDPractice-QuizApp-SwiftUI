286fadc7df9b2217b6b3c92a16e1c1e20b6d15e9
Everest Liu <ev@logik-m2-mini.local>
2023-09-05 19:02:53 -0700

    QuizApp - UITableView testing

--- QuizApp/QuizApp/AppDelegate.swift
+++ QuizApp/QuizApp/AppDelegate.swift
@@ -12,6 +12,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	var window: UIWindow?
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		let window = UIWindow(frame: UIScreen.main.bounds)
+		let viewController = QuestionViewController(question: "A question?", options: ["Option 1", "Option 2"], selection: { selected in
+			print(selected)
+		})
+
+		viewController.loadViewIfNeeded()
+		viewController.tableView.allowsMultipleSelection = false
+
+		window.rootViewController = viewController
+
+		self.window = window
+		window.makeKeyAndVisible()
+
 		return true
 	}
 }
--- QuizApp/QuizApp/QuestionViewController.swift
+++ QuizApp/QuizApp/QuestionViewController.swift
@@ -13,10 +13,10 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
 
 	private var question = ""
 	private var options = [String]()
+	private var selection: (([String]) -> Void)? = nil
 	private var reuseIndentifier = "Cell"
-	private var selection: ((String) -> Void)? = nil
 
-	convenience init(question: String, options: [String], selection: @escaping (String) -> Void) {
+	convenience init(question: String, options: [String], selection: @escaping ([String]) -> Void) {
 		self.init()
 		self.question = question
 		self.options = options
@@ -39,9 +39,21 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
 
 		return cell
 	}
-	
+
 	func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-		selection?(options[indexPath.row])
+		selection?(selectedOptions(in: tableView))
+	}
+
+	func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+		if tableView.allowsMultipleSelection {
+			selection?(selectedOptions(in: tableView))
+		}
+	}
+
+	private func selectedOptions(in tableView: UITableView) -> [String] {
+		guard let indexPaths = tableView.indexPathsForSelectedRows else { return [] }
+
+		return indexPaths.map { options[$0.row] }
 	}
 
 	private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
