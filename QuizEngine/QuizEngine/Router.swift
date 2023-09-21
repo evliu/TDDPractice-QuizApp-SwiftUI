5aad52fb8e9e37f9d842cc5c54fea4fe89a97a94
Ev Liu <azn_logik@yahoo.com>
2023-09-20 20:49:47 -0700

    QuizEngine - start refactoring Router to QuizDelegate

--- QuizEngine/QuizEngine/Router.swift
+++ QuizEngine/QuizEngine/Router.swift
@@ -7,6 +7,15 @@
 
 import Foundation
 
+public protocol QuizDelegate {
+	associatedtype Question: Hashable
+	associatedtype Answer
+
+	func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
+	func routeTo(result: Result<Question, Answer>)
+}
+
+@available(*, deprecated)
 public protocol Router {
 	associatedtype Question: Hashable
 	associatedtype Answer
@@ -14,3 +23,8 @@ public protocol Router {
 	func routeTo(question: Question, answerCallback: @escaping (Answer) -> Void)
 	func routeTo(result: Result<Question, Answer>)
 }
+
+/** TODO:
+ 1. add message for deprecated protocol
+ 2. remove Hashable from Question; make Result type Generic
+ */
